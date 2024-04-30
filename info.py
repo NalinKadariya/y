522478324d8fea119acf3a4a8ee2ea0e45f0ec88
NalinKadariya <nali0074@edu.nextkbh.dk>
2024-04-30 12:31:52 +0200

    sserver

--- info.py
+++ info.py
@@ -6,8 +6,8 @@ gmailLengthMax = 100
 openaiApiKeyLengthMax = 100
 app_title = "Y"
 logo_path = "Images/Logo.ico"
-IP = "localhost"
-PORT = 5000
+IP = "34.159.114.14"
+PORT = 6969
 
 # Splash Info
 Splash_time = 5 # seconds
